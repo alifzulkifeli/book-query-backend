19cbca57f95d600a8f12807d20fff1504a7caf18
aliff <alifzulkifeli@gmail.com>
2020-06-23 02:13:28 +0900

    set up for deployment

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var request = require("request");
 var cheerio = require("cheerio");
 const app = express();
 const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
 
 app.use(express.json());
+app.use(helmet());
+app.use(compression());
 app.use(morgan("tiny"));
 
 app.post("/get-books", async (req, res) => {
